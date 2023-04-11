d8a9e4e50b132a3f3bcef9d62332a99f1fbab1ae
Mar12358 <120040536+Mar12358@users.noreply.github.com>
2023-04-11 12:43:54 -0300

    Destructure assignment to nested categories

--- main.js
+++ main.js
@@ -38,30 +38,31 @@ function popupWindow(project) {
   togglePopUp();
   console.log(project);
   popupWindowDiv.children[0].children[0].children[0].innerHTML = project.name;
-  popupWindowDiv.children[0].children[1].children[0].children[0].innerHTML = project.categ[0];
-  popupWindowDiv.children[0].children[1].children[2].children[0].innerHTML = project.categ[1];
-  popupWindowDiv.children[0].children[1].children[4].children[0].innerHTML = project.categ[2];
+  const [categ1, categ2, categ3] = project.categ;
+  popupWindowDiv.children[0].children[1].children[0].children[0].innerHTML = categ1;
+  popupWindowDiv.children[0].children[1].children[2].children[0].innerHTML = categ2;
+  popupWindowDiv.children[0].children[1].children[4].children[0].innerHTML = categ3;
   popupWindowDiv.children[0].children[2].children[0].src = project.mobileImage;
   popupWindowDiv.children[0].children[3].innerHTML = project.mobileDescription;
-  console.log(project.technologies.length)
-  popupWindowDiv.children[0].children[4].innerHTML = '<li><p>'+project.technologies[0]+'</p></li>';
+  console.log(project.technologies.length);
+  popupWindowDiv.children[0].children[4].innerHTML = `<li><p>${project.technologies[0]}</p></li>`;
   let cantCateg = 3;
 
   if (window.matchMedia('(min-width: 768px)').matches) {
     cantCateg = project.technologies.length;
   }
   for (let i = 1; i < cantCateg; i += 1) {
-    popupWindowDiv.children[0].children[4].insertAdjacentHTML('beforeend','<li><p>'+project.technologies[i]+'</p></li>')
+    popupWindowDiv.children[0].children[4].insertAdjacentHTML('beforeend', `<li><p>${project.technologies[i]}</p></li>`);
   }
 }
 
 const projects = document.querySelectorAll('.card_image');
 const buttons = document.querySelectorAll('.see_project_button');
 for (let i = 0; i < buttons.length; i += 1) {
-  let projectId = projectList.find((p) => p.id === projects[i].alt)
+  const projectId = projectList.find((p) => p.id === projects[i].alt);
   buttons[i].addEventListener('click', popupWindow.bind(null, projectId));
   projects[i].addEventListener('click', popupWindow.bind(null, projectId));
   // console.log(projects[i].alt);
 }
-const closePopup = document.querySelector('.imgButtonClose')
+const closePopup = document.querySelector('.imgButtonClose');
 closePopup.addEventListener('click', togglePopUp);
